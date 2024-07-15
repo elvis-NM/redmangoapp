581eb1d7a0966ccfabaf86ee8e61071ce979a5f3
Elvis Makia <elvis_makia@yahoo.com>
2024-07-15 15:46:56 -0400

    Api configuration

---
+++ src/Apis/index.ts
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const menuItemApi = createApi({
+    reducerPath: "menuItemApi",
+    baseQuery: fetchBaseQuery({
+      baseUrl: "https://redmangoapi.azurewebsites.net/api/",
+      prepareHeaders: (headers: Headers, api) => {
+        const token = localStorage.getItem("token");
+        token && headers.append("Authorization", "Bearer " + token);
+      },
+    }),
+    tagTypes: ["MenuItems"],
+    endpoints: (builder) => ({
+      getMenuItems: builder.query({
+        query: () => ({
+          url: "menuitem",
+        }),
+        providesTags: ["MenuItems"],
+      }),
+      getMenuItemById: builder.query({
+        query: (id) => ({
+          url: `menuitem/${id}`,
+        }),
+        providesTags: ["MenuItems"],
+      }),
+     
+    }),
+  });
+  
+  export const {
+    useGetMenuItemsQuery,
+    useGetMenuItemByIdQuery
+  } = menuItemApi;
+  export default menuItemApi;
