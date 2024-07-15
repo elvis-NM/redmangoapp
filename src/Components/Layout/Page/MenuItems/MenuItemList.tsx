541575055ad7b7abf220659472290a47421c0311
Elvis Makia <elvis_makia@yahoo.com>
2024-07-15 16:26:43 -0400

    consume api

--- src/Components/Layout/Page/MenuItems/MenuItemList.tsx
+++ src/Components/Layout/Page/MenuItems/MenuItemList.tsx
@@ -2,17 +2,33 @@ import React from 'react'
 import{useState,useEffect} from "react";
 import { menuItemModel } from '../../../../Interfaces';
 import MenuItemCard from './MenuItemCard';
+import { useGetMenuItemsQuery } from '../../../../Apis/menuItemApi';
+import { useDispatch } from 'react-redux';
+import { setMenuItem } from '../../../../Storage/Redux/menuItemSlice';
 
 
 
 
 function MenuItemList() {
-    const[menuItems,setMenuItems] =useState<menuItemModel[]>([]);
+    //const[menuItems,setMenuItems] =useState<menuItemModel[]>([]);
+    const dispatch =useDispatch();
+    const {data,isLoading} = useGetMenuItemsQuery(null);
+    
 
-  useEffect(() =>{fetch("https://redmangoapi.azurewebsites.net/api/MenuItem").then((response)=>response.json()).then((data)=>{console.log(data);setMenuItems(data.result)})},[])
+  //useEffect(() =>{fetch("https://redmangoapi.azurewebsites.net/api/MenuItem").then((response)=>response.json()).then((data)=>{console.log(data);setMenuItems(data.result)})},[])
+  useEffect(() => {
+    if (!isLoading) {
+      dispatch(setMenuItem(data.result));
+      }
+      },[isLoading]);
+
+      if(isLoading){
+        return<div>Loading...</div>;
+      }
+
   return (
     <div className="container row">
-      {menuItems.length>0 && menuItems.map((menuItem,index)=>(<MenuItemCard menuItem={menuItem} key={index}/>))}
+      {data.result.length>0 && data.result.map((menuItem:menuItemModel,index:number)=>(<MenuItemCard menuItem={menuItem} key={index}/>))}
     </div>
   )
 }
