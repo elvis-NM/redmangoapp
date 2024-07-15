ce15a6a613ee50632f8a75088d107d40ed780f75
Elvis Makia <elvis_makia@yahoo.com>
2024-07-14 21:27:04 -0400

    typescrpt basic file structure config

---
+++ src/Container/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Header,Footer} from "../Components/Layout";
+
+
+function App() {
+  return (
+    <div className="text-success"><Header/>Main Component<Footer/>
+    </div>
+  );
+}
+
+export default App;
