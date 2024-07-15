cea5b60ad81cfc4a56b182c57593cc6117fd5615
Elvis Makia <elvis_makia@yahoo.com>
2024-07-15 08:55:12 -0400

    react-typscript data fetch hook implementation

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Header,Footer} from "../Components/Layout";
+import{useState,useEffect} from "react";
 
 
 function App() {
+  const[menuItems,setMenuItems] =useState([]);
+
+  useEffect(() =>{fetch("https://redmangoapi.azurewebsites.net/api/MenuItem").then((response)=>response.json()).then((data)=>{console.log(data);})})
   return (
     <div className="text-success"><Header/>Main Component<Footer/>
     </div>
