d788ec1179325b9b0c210d9b9b79eecff455aa35
Elvis Makia <elvis_makia@yahoo.com>
2024-07-15 09:58:16 -0400

    card component and data display

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Header,Footer} from "../Components/Layout";
 import{useState,useEffect} from "react";
+import { menuItemModel } from '../Interfaces';
+import { Home } from '../Pages';
 
 
 function App() {
-  const[menuItems,setMenuItems] =useState([]);
+  const[menuItems,setMenuItems] =useState<menuItemModel[]>([]);
 
-  useEffect(() =>{fetch("https://redmangoapi.azurewebsites.net/api/MenuItem").then((response)=>response.json()).then((data)=>{console.log(data);})})
+  useEffect(() =>{fetch("https://redmangoapi.azurewebsites.net/api/MenuItem").then((response)=>response.json()).then((data)=>{console.log(data);setMenuItems(data.result)})},[])
   return (
-    <div className="text-success"><Header/>Main Component<Footer/>
+    <div className="text-success"><Header/><Home/><Footer/>
     </div>
   );
 }
